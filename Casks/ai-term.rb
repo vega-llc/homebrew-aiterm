a3b04cbd97ae1ab33f62d53229543c27b299d498
DigitalKredit <147826338+DRVBSS@users.noreply.github.com>
2026-08-10 18:38:05 -0400

    AITerm cask 0.19.1

--- Casks/ai-term.rb
+++ Casks/ai-term.rb
@@ -1,6 +1,6 @@
 cask "ai-term" do
-  version "0.19.0"
-  sha256 "e84d9a5902022e1402a988e430e0a61271ed4bb0276f0e9adb8e624acb9059aa"
+  version "0.19.1"
+  sha256 "3436510b2624f1dc2bfbe398bb543e524516c0ceee463c83a5eebf81318c1ffa"
 
   url "https://aiterm-dl.vega-llc.workers.dev/AITerm-#{version}.dmg",
       verified: "aiterm-dl.vega-llc.workers.dev/"
